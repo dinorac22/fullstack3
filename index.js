b61c2b41ba1998e587021bec6119ed086a1c8f3e
dinorac22 <dinorac22@gmail.com>
2025-12-04 15:19:40 -0500

    Ejercicio 3.6 - Parte 3

--- index.js
+++ index.js
@@ -45,6 +45,12 @@ app.delete('/api/persons/:id', (req, res) => {
 
 app.post('/api/persons', (req, res) => {
   const body = req.body;
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: 'Name or number is missing' });
+  }
+  if (persons.find(p => p.name === body.name)) {
+    return res.status(400).json({ error: 'Name must be unique' });
+  }
 
   const person = {
     id: Math.floor(Math.random() * 10000000),
