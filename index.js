1b7b618daa87f14d3fd00905f11290c4ce4f4df4
dinorac22 <dinorac22@gmail.com>
2025-12-04 15:10:20 -0500

    Ejercicio 3.5 - Parte 3

--- index.js
+++ index.js
@@ -43,6 +43,21 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end();
 });
 
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+
+  const person = {
+    id: Math.floor(Math.random() * 10000000),
+    name: body.name,
+    number: body.number
+  };
+
+  persons = persons.concat(person);
+  res.json(person);
+});
+
+  
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server is running at http://localhost:${PORT}/`)
